fee067e773e3e4f3ed0be90d32cebba5643e2b40
WodaMineralna <raknieborak65@gmail.com>
2025-05-26 13:58:28 +0200

    Separating slices into separate files
    - adjusted all of the Components, so they import accordingly

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 
-import { authActions } from "../store";
+import { authActions } from "../store/auth-slice.js";
 import classes from "./Auth.module.css";
 
 const Auth = () => {
